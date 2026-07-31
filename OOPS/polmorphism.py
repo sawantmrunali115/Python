216654d0ff30cc8c65d44aaf7c8f659d6d4fa852
sawantmrunali115 <sawantmrunali115@gmail.com>
2026-07-31 20:03:13 +0530

    OOPS Practice

--- OOPS/polmorphism.py
+++ OOPS/polmorphism.py
@@ -1,2 +1,24 @@
 # Example of Polymorphism in Python
 
+class Payment:
+    def pay(self, amount):
+        print("Payment processing for the amount:", amount)
+
+
+class CreditCardPayment(Payment):
+    def pay(self, amount):
+        print("CREDIT CARD PAYMENT:", amount)
+
+class UPI(Payment):
+    def pay(self, amount):
+        print("UPI PAYMENT:", amount)   
+
+class CashPayment(Payment):
+    def pay(self, amount):
+        print("CASH PAYMENT:", amount)
+
+payments = [CreditCardPayment(), UPI(), CashPayment()]
+
+for payment in payments:
+    payment.pay(100)  # Each payment method processes the payment differently
+
