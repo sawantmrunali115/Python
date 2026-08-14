8ccd6119948b6ca8a64972f2ce37ac7ab0b3676d
sawantmrunali115 <sawantmrunali115@gmail.com>
2026-08-14 08:29:31 +0530

    Modules

---
+++ Modules/perform_calc.py
@@ -0,0 +1,13 @@
+import calculations as calc
+
+# Calling the add function from calculations module
+print(calc.add(5, 3))  
+
+# Calling the subtract function from calculations module
+print(calc.subtract(15, 8))  
+
+# Calling the multiply function from calculations module with default argument
+print(calc.multiply(4))  
+
+# Calling the divide function from calculations module
+print(calc.divide(10, 2))  
